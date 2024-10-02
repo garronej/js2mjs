9d5386ec961a4e9522c96c302f8c1ff8aa5f4b64
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-10-02 07:30:33 +0200

    Update to generate sourcemaps and update build

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # js2mjs
 
-Convert a ESM distribution using .js extension to an ESM distribution using .mjs extension.
+Convert a ESM distribution that is using .js extension to an ESM distribution using .mjs extension.
+
+## Motivation
+
+Node.js is ESM compatible but it's a bit picky about the files extension. It expects import statements to include the file extension
+and that `.mjs` be used instead of `.js`.  
+So when you setup TypeScript to output an ESM distribution it will generate a distribution that isn't compatible with Node in `"type": "module"` mode.  
+It's an ESM distribution that Vite, Next.js and other build tools will understand but not Node.  
+This module let you patch the distribution so it can be used with node.
 
 ```bash
 yarn add --dev js2mjs
+npx tsc # Build your project we assume it generate dist/esm
 npx js2mjs dist/esm # Your ESM distribution using .js extension will be updated to an ESM distribution using .mjs extension
 ```
---
+++ scripts/build.ts
@@ -0,0 +1,45 @@
+import * as child_process from "child_process";
+import * as fs from "fs";
+import { join } from "path";
+import { assert } from "tsafe/assert";
+import chalk from "chalk";
+import { same } from "evt/tools/inDepth/same";
+
+console.log(chalk.cyan("Building js2mjs..."));
+
+const startTime = Date.now();
+
+if (fs.existsSync(join("dist", "bin", "main.original.js"))) {
+    fs.renameSync(join("dist", "bin", "main.original.js"), join("dist", "bin", "main.js"));
+}
+
+if (fs.existsSync(join("dist", "index.original.js"))) {
+    fs.renameSync(join("dist", "index.original.js"), join("dist", "index.js"));
+}
+
+run("npx tsc");
+
+if (!fs.readFileSync(join("dist", "bin", "main.js")).toString("utf8").includes("__nccwpck_require__")) {
+    fs.cpSync(join("dist", "bin", "main.js"), join("dist", "bin", "main.original.js"));
+}
+
+run(`npx ncc build ${join("dist", "bin", "main.js")} -o ${join("dist", "ncc_out")}`);
+
+assert(same(fs.readdirSync(join("dist", "ncc_out")), ["index.js"]));
+
+fs.cpSync(join("dist", "ncc_out", "index.js"), join("dist", "bin", "main.js"));
+
+fs.rmSync(join("dist", "ncc_out"), { recursive: true });
+
+fs.chmodSync(
+    join("dist", "bin", "main.js"),
+    fs.statSync(join("dist", "bin", "main.js")).mode | fs.constants.S_IXUSR | fs.constants.S_IXGRP | fs.constants.S_IXOTH
+);
+
+console.log(chalk.green(`✓ built in ${((Date.now() - startTime) / 1000).toFixed(2)}s`));
+
+function run(command: string) {
+    console.log(chalk.grey(`$ ${command}`));
+
+    child_process.execSync(command, { stdio: "inherit" });
+}
